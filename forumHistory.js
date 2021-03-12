ceafe4266ed023e49f56a12ec273de974d7a339e
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-12 21:59:47 +0100

    Generate yearly lists of events in DOM sections using json data. Clean js code.

--- forumHistory.js
+++ forumHistory.js
@@ -1,36 +1,63 @@
 const sectionElements = document.querySelectorAll("section");
 const navigationElements = document.querySelectorAll("#navigation > ul > li");
 
-sectionElements.forEach((section) => console.log(section.id));
-
 const historyDataURL = "./history-data/history.json";
-let history = {};
-let selectedSectionID;
+
+let topicUrlPath = "";
+let poolUrlPath = "";
 
 // Get History Data
-function getHistoryData(dataURL) {
-    fetch(dataURL)
+async function getHistoryData(dataURL) {
+    await fetch(dataURL)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            history = data;
-            console.log(history);
+            topicUrlPath = data["topicUrlPath"];
+            poolUrlPath = data["poolUrlPath"];
+            generateHistoryYearSections(data);
         });
 }
 
-function selectHistoryYear(year) {
-    console.log(year);
+function buildHistoryListItem(forumHistoryYear, list) {
+    forumHistoryYear.map((event, index) => {
+        const { date, id, title, type, addition } = event;
+        const listItem = document.createElement("ul");
+        listItem.classList.add("history__event");
+        const dateSpan = document.createElement("span");
+        dateSpan.classList.add("history__event-date");
+        dateSpan.textContent = date;
+        const topicLink = document.createElement("a");
+        topicLink.classList.add("history__event-link");
+        if (!type) {
+            topicLink.href = `${topicUrlPath}${id}`;
+        } else {
+            topicLink.href = `${poolUrlPath}${id}`;
+        }
+        topicLink.textContent = title;
+        listItem.append(dateSpan, topicLink);
+        if (addition) {
+            const additionSpan = document.createElement("span");
+            additionSpan.classList.add("history__event-addition");
+            listItem.appendChild(additionSpan);
+        }
+
+        list.appendChild(listItem);
+    });
 }
 
-// Event Listeners
-navigationElements.forEach((navEl) => {
-    navEl.addEventListener("click", () =>
-        selectHistoryYear(navEl.dataset.year)
-    );
-});
+function generateHistoryYearSections(forumHistory) {
+    if (forumHistory) {
+        sectionElements.forEach((section, index) => {
+            const list = section.querySelector(".history__text-list");
+            if (Object.keys(forumHistory).includes(section.id)) {
+                buildHistoryListItem(forumHistory[section.id], list);
+            } else {
+                console.log("Nie znaleziono danych pod pobranym kluczem");
+            }
+        });
+    }
+}
 
 // On Load
 getHistoryData(historyDataURL);
-
-export default forumHistory;
