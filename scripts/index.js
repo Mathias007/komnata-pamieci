b708477f3eb657fca7f9a78a057fed5c6266d0be
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-13 17:05:15 +0100

    add opacity change by scrolling.

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,25 @@ menuBars.addEventListener("click", () => toggleNav(menuBars, overlay));
 navigationElements.forEach((navEl) => {
     navEl.addEventListener("click", () => toggleNav(menuBars, overlay));
 });
+
+// Handle scrolling page
+
+let opacity;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+
+    document.querySelectorAll("section").forEach((section, index) => {
+        let scrolledOpacity =
+            currentScroll / section.offsetTop !== 0
+                ? currentScroll / section.offsetTop
+                : 1;
+        if (section.offsetTop - currentScroll <= 0) {
+            opacity = 1;
+            section.style.opacity = opacity;
+        } else {
+            opacity = scrolledOpacity <= 1 ? scrolledOpacity : 1;
+            section.style.opacity = opacity;
+        }
+    });
+});
