97299145252c1eeae041140a16d40dd0497599be
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-13 14:08:15 +0100

    generate proper topicLink and additional span if needed.

--- scripts/forumHistory.js
+++ scripts/forumHistory.js
@@ -18,24 +18,26 @@ export async function getHistoryData(dataURL) {
 
 function buildHistoryListItem(forumHistoryYear, list) {
     forumHistoryYear.map((event, index) => {
-        const { date, id, title, type, addition } = event;
-        const listItem = document.createElement("ul");
+        const { date, id, title, addition } = event;
+        const listItem = document.createElement("li");
         listItem.classList.add("history__event");
         const dateSpan = document.createElement("span");
         dateSpan.classList.add("history__event-date");
         dateSpan.textContent = date;
         const topicLink = document.createElement("a");
         topicLink.classList.add("history__event-link");
-        if (!type) {
-            topicLink.href = `${topicUrlPath}${id}`;
-        } else {
+        const separator = " - ";
+        if (event.hasOwnProperty("type")) {
             topicLink.href = `${poolUrlPath}${id}`;
+        } else {
+            topicLink.href = `${topicUrlPath}${id}`;
         }
         topicLink.textContent = title;
-        listItem.append(dateSpan, topicLink);
-        if (addition) {
+        listItem.append(dateSpan, separator, topicLink);
+        if (event.hasOwnProperty("addition")) {
             const additionSpan = document.createElement("span");
             additionSpan.classList.add("history__event-addition");
+            additionSpan.textContent = addition;
             listItem.appendChild(additionSpan);
         }
 
